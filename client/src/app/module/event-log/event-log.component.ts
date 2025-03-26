218fc3ec7eb6b71f6bf53949e0a0287ad9558e2d
DannyPhang <dannyphang64@gmail.com>
2025-03-26 15:57:08 +0800

    feat: add event search functionality and enhance event log component

--- client/src/app/module/event-log/event-log.component.ts
+++ client/src/app/module/event-log/event-log.component.ts
@@ -3,8 +3,8 @@ import { EventLogDto, EventService } from '../../core/services/event.service';
 import { ToastService } from '../../core/services/toast.service';
 import { ROW_PER_PAGE_DEFAULT_LIST, ROW_PER_PAGE_DEFAULT } from '../../core/shared/constants/common.constants';
 import { Table } from 'primeng/table';
-import { FormControl } from '@angular/forms';
-import { OptionsModel } from '../../core/services/components.service';
+import { FormControl, FormGroup } from '@angular/forms';
+import { CONTROL_TYPE, FormConfig, OptionsModel } from '../../core/services/components.service';
 import { debounceTime } from 'rxjs';
 
 interface Column {
@@ -27,6 +27,24 @@ export class EventLogComponent {
   searchOptionsFormControl: FormControl = new FormControl('eventName');
   searchOptions: OptionsModel[] = [];
   cols!: Column[];
+  isSearchCollapsed: boolean = true;
+  advancedSearchFormConfig: FormConfig[] = [];
+  advancedSearchFormGroup: FormGroup = new FormGroup({
+    uid: new FormControl(),
+    project: new FormControl(),
+    eventName: new FormControl(),
+    description: new FormControl(),
+    module: new FormControl(),
+    modifiedDate: new FormControl(
+      (() => {
+        const yesterday = new Date();
+        yesterday.setDate(yesterday.getDate() - 1);
+        return yesterday;
+      })() // Immediately Invoked Function Expression (IIFE)
+    ),
+    modifiedBy: new FormControl()
+  });
+
 
   constructor(
     private eventService: EventService,
@@ -96,8 +114,76 @@ export class EventLogComponent {
     this.searchFormControl.valueChanges.pipe(
       debounceTime(500)
     ).subscribe(value => {
-      console.log(value)
+      this.searchEvents();
     });
+
+    this.advancedSearchFormConfig = [
+      {
+        label: "UID",
+        type: CONTROL_TYPE.Textbox,
+        layoutDefine: {
+          row: 0,
+          column: 0,
+        },
+        fieldControl: this.advancedSearchFormGroup.get('uid'),
+      },
+      {
+        label: "Project",
+        type: CONTROL_TYPE.Textbox,
+        layoutDefine: {
+          row: 0,
+          column: 1,
+        },
+        fieldControl: this.advancedSearchFormGroup.get('project'),
+      },
+      {
+        label: "Event Name",
+        type: CONTROL_TYPE.Textbox,
+        layoutDefine: {
+          row: 0,
+          column: 2,
+        },
+        fieldControl: this.advancedSearchFormGroup.get('eventName'),
+      },
+      {
+        label: "Description",
+        type: CONTROL_TYPE.Textbox,
+        layoutDefine: {
+          row: 1,
+          column: 0,
+        },
+        fieldControl: this.advancedSearchFormGroup.get('description'),
+      },
+      {
+        label: "Module",
+        type: CONTROL_TYPE.Textbox,
+        layoutDefine: {
+          row: 1,
+          column: 1,
+        },
+        fieldControl: this.advancedSearchFormGroup.get('module'),
+      },
+      {
+        label: "Updated Date Time",
+        type: CONTROL_TYPE.Calendar,
+        layoutDefine: {
+          row: 1,
+          column: 2,
+        },
+        fieldControl: this.advancedSearchFormGroup.get('modifiedDate'),
+        showTime: true
+      },
+      {
+        label: "User",
+        type: CONTROL_TYPE.Textbox,
+        layoutDefine: {
+          row: 2,
+          column: 0,
+          colSpan: 4
+        },
+        fieldControl: this.advancedSearchFormGroup.get('modifiedBy'),
+      }
+    ];
   }
 
   clear(table: Table) {
@@ -107,5 +193,59 @@ export class EventLogComponent {
 
   formatDate(dateValue: string) {
     return new Date(dateValue);
+  }
+
+  advancedFilter() {
+    this.isSearchCollapsed = !this.isSearchCollapsed;
+    this.clearFilters();
+  }
+
+  clearFilters() {
+    this.advancedSearchFormGroup.reset({ emitEvent: false });
+  }
+
+  searchBtn() {
+    this.searchEvents();
+  }
+
+  searchEvents() {
+    let params: any = {};
+
+    // Dynamically add selected search option (dropdown value) and input field value
+    const selectedKey = this.searchOptionsFormControl.value;
+    if (this.searchFormControl.value && selectedKey) {
+      params[selectedKey] = this.searchFormControl.value;
+    }
+    else if (Object.values(this.advancedSearchFormGroup.value).some(value => value !== null && value !== undefined && value !== "")) {
+      const formValues = this.advancedSearchFormGroup.value;
+
+      // Remove null, undefined, and empty string values
+      const filteredValues = Object.fromEntries(
+        Object.entries(formValues).filter(([_, value]) => value !== null && value !== undefined && value !== "")
+      );
+      params = filteredValues;
+    }
+
+    // Call searchEvents function with updated params
+    this.eventService.searchEvents(params).subscribe({
+      next: response => {
+        if (response.isSuccess) {
+          this.eventList = response.data;
+        }
+        else {
+          this.toastService.addSingle({
+            message: response.responseMessage,
+            severity: 'error',
+          });
+        }
+      },
+      error: error => {
+        console.log(error.error.responseMessage)
+        this.toastService.addSingle({
+          message: error.error.responseMessage,
+          severity: 'error',
+        });
+      }
+    });
   }
 }
